2933391504b1fc39bb292f55047b3ca2d667a6bb
Norly Pierre <norlypierre@gmail.com>
2025-10-28 14:32:57 -0400

    Asset: Constructor - Get/Set - return double

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,7 @@
 package com.pluralsight;
 
-public class House {
+public class House extends Asset {
+    public House(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
 }
---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class Vehicle extends Asset {
+
+    public Vehicle(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+}
