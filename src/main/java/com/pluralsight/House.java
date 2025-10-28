81563059a439a15e60ce68ee61a995762edc8341
Norly Pierre <norlypierre@gmail.com>
2025-10-28 15:04:14 -0400

    House and vehicle setup

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,7 +1,49 @@
 package com.pluralsight;
 
 public class House extends Asset {
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
     public House(String description, String dateAcquired, double originalCost) {
         super(description, dateAcquired, originalCost);
+
+    }
+    @Override
+    public double getValue(){
+        return 0;
     }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,6 +1,9 @@
 package com.pluralsight;
 
 public class Vehicle extends Asset {
+    private String makeModel;
+    private int year;
+    private int odometer;
 
     public Vehicle(String description, String dateAcquired, double originalCost) {
         super(description, dateAcquired, originalCost);
